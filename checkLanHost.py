e8a13c7ad2bc47cd32e76a2b71181c5dc7547dea
Michelle Novenda Hartono <mnhartono@gmail.com>
2022-04-27 12:17:34 -0500

    Add files via upload

--- checkLanHost.py
+++ checkLanHost.py
@@ -1,36 +1,61 @@
 import nmap
 from datetime import datetime
 from netaddr import IPAddress
-from tracert import *
+from trace import *
 
-trace = Trace()
+class CheckLanHost:
+    def __init__(self, os):
+        self.os = os
 
-def callback_result(host, scan_result):
-    print('------------------')
-    print(host, scan_result)
+    def callback_result(self, host, scan_result):
+        if self.os == "win":
+            print('------------------')
+            print("scan_res:", scan_result)
+            if scan_result['scan'] != {}:
+                print(scan_result['scan'])
+            else:
+                print(host)
+        elif self.os == "linux":
+            print('------------------')
+            print("scan_res:", scan_result)
+            if scan_result != None and scan_result['scan'] != None:
+                print(scan_result['scan'])
+            else:
+                print(host)
+        else:
+            print("err")
 
-def LanHost(config, os):
-    wireless_lan_gateway = config.getGateway11()
-    wireless_lan_subnet = config.getSubnet11()
-    if os == 'win':
-        mask_num = IPAddress(wireless_lan_subnet).netmask_bits()
-    elif os == 'linux':
-        mask_num = wireless_lan_subnet
-    else:
-        mask_num=-1
+    def AllLanHost(self, config):
+        trace = Trace(self.os)
+        wireless_lan_gateway = config.getGateway11()
+        wireless_lan_subnet = config.getSubnet11()
+        if self.os == 'win':
+            mask_num = IPAddress(wireless_lan_subnet).netmask_bits()
+        elif self.os == 'linux':
+            mask_num = wireless_lan_subnet
+        else:
+            mask_num=-1
 
-    ipAddr = wireless_lan_gateway + '/' + str(mask_num)
-    print(ipAddr)
+        # cmd = input("Choose an option: '-sS'/'-sP'/'-sL'/'-PS'/'-PU': ")
+        cmd = '-sS -F -O -T4' 
+        cmd+=" --min-hostgroup 20"
+        # the fastest parameters I find so far
+        # cmd+=" --min-rate 10"
 
+        start_time = datetime.now()
+        nma = nmap.PortScannerAsync()
+        
+        for i in range(len(trace.IPlist)):
+            if i != 0 and trace.IPlist[i-1] == trace.root_IP: # stop at root IP
+                break
 
-    cmd = input("Choose an option: '-sS'/'-sP'/'-sL'/'-PS'/'-PU': ")
-    start_time = datetime.now()
-    nma = nmap.PortScannerAsync()
-    IPdict = trace.IPdict
-    privateIP_list = IPdict['private']
-    for ip in privateIP_list:
-        nma.scan(hosts=ip, arguments=cmd, callback=callback_result)
-        while nma.still_scanning():
-            nma.wait(2)
-    end_time = datetime.now()
-    print("Duration: {}".format(end_time - start_time))
+            ip=trace.IPlist[i]
+            mask_num = 24 if ip != wireless_lan_gateway else mask_num
+            ip = ip + '/' + str(30)
+
+            print(f"scanning LAN under {ip}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            nma.scan(hosts=ip, arguments=cmd, callback=self.callback_result)
+            while nma.still_scanning():
+                nma.wait(2)
+        end_time = datetime.now()
+        print("Duration: {}".format(end_time - start_time))
--- test.py
+++ test.py
@@ -1,43 +1,59 @@
-import sys
-from ipaddress import ip_address
-from subprocess import Popen, PIPE
+import nmap
+from datetime import datetime
+from netaddr import IPAddress
+from trace import *
 
+# trace = Trace()
+class Test:
+    def __init__(self, os):
+        self.user_os = os
 
-def getLANRouters(tracertList):  # Get the direct ancestors in LAN
-    ip_list = []
-    for i in range(len(tracertList)):
-        # some IP is in [], we have to take it out
-        if (tracertList[i] != '' and tracertList[i][0] == '[') or (tracertList[i] != '' and tracertList[i][0] == '('  and tracertList[i][-1] == ')'):
-            ip_list.append(tracertList[i][1:-1])
-
-        if(all([item.isnumeric() for item in tracertList[i].split('.')]) and
-                len(tracertList[i].split('.')) == 4):  # if the string is in format of IP addr
-            ip_list.append(tracertList[i])  # append it into ip_list
-
-    ip_list.pop(0)
-    IP={'private': [], 'public':[]}
-    for ip in ip_list:
-        if(ip_address(ip).is_private):
-            IP['private'].append(ip)
+    def callback_result(self, host, scan_result):
+        if self.user_os == "win":
+            print('------------------')
+            print("scan_res:", scan_result)
+            if scan_result['scan'] != {}:
+                print(scan_result['scan'])
+            else:
+                print(host)
+        elif self.user_os == "linux":
+            print('------------------')
+            print("scan_res:", scan_result)
+            if scan_result != None and scan_result['scan'] != None:
+                print(scan_result['scan'])
+            else:
+                print(host)
         else:
-            IP['public'].append(ip)
+            print("err")
 
-    return IP,ip_list
+    def AllLanHostTest(self, os):
+        wireless_lan_gateway = "140.114.71.253"
+        wireless_lan_subnet = "255.255.255.0"
+        if os == 'win':
+            mask_num = IPAddress(wireless_lan_subnet).netmask_bits()
+        elif os == 'linux':
+            mask_num = wireless_lan_subnet
+        else:
+            mask_num=-1
 
-    
-if len(sys.argv) < 2:
-    print('Usage:python tracert.py "command to watch"\n\
-Example:python tracert.py tracert 192.168.0.1\n')
-    sys.exit(1)
+        # cmd = input("Choose an option: '-sS'/'-sP'/'-sL'/'-PS'/'-PU': ")
+        cmd = '-sS -F -O -T4'
+        # cmd+=" --min-paralleism 10"
+        cmd+=" --min-hostgroup 20"
+        # cmd+=" --min-rate 10"
+        # cmd+=" --max-scan-delay 500ms" 
+        # cmd+=" --host-timeout 100ms" # 100ms is too short, some msg lost
+        start_time = datetime.now()
+        nma = nmap.PortScannerAsync()
+        # privateIP_list = trace.IPdict['private']
+        privateIP_list = [wireless_lan_gateway]
+        for ip in privateIP_list:
+            mask_num = 16 if ip != wireless_lan_gateway else mask_num
+            ip = ip + '/' + str(mask_num)
 
-cmdline_ins = sys.argv[1:]
-print("cmdline", cmdline_ins)
-p = Popen(cmdline_ins, stdout=PIPE)
-print("p", p)
-outputText = str(p.communicate()[0]).split(' ')
-print(outputText)
-
-print("\nIP:")
-b,a=getLANRouters(outputText)
-print(a)
-print(b)
+            print(f"scanning LAN under {ip}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            nma.scan(hosts=ip, arguments=cmd, callback=self.callback_result)
+            while nma.still_scanning():
+                nma.wait(2)
+        end_time = datetime.now()
+        print("Duration: {}".format(end_time - start_time))
