419b233fc15336e07f0ea9303a70f8d614433cf5
michellenovenda <mnhartono@gmail.com>
2022-06-18 22:00:51 -0500

    modify: test.py && test.yml

--- test.py
+++ test.py
@@ -10,38 +10,34 @@ import yaml
 #             atk_chain = yaml.load(attack_chain, Loader=yaml.SafeLoader)
 #             class_chain, block_chain= atk_chain["class_chain"], atk_chain["block_chain"]
 
+class Error(Exception):
+    '''Base class for customized exceptions'''
+    pass
+
+class RuleLengthError(Error):
+    '''To raise an exception when there is more than one rule in a single yaml file.'''
+    pass
+
 def solve(rule):
-    # print("typeevalrule:", type(eval(rule)))
-    # print("typeTrue:", type(True))
     if isinstance(rule, str) and type(eval(rule)) == Boolean:
         return eval(rule)
     print("call solve!",rule)
     for i,(key,val) in enumerate(rule.items()):
         print(f"key:{key}, val:{val}")
-        # print(type(val))
         if isinstance(val, list):
             result = (key=='and')
             for item in val:
-                # print("items:", item)
                 if isinstance(item, dict):
                     outcome = solve(item)
                 else:
-                    # print("inn")
                     outcome = eval(item)
-                    # print("outcomee:", outcome)
                 
                 if key == 'or':
-                    # print("result1bef:", result)
                     result = (outcome or result)
-                    # print("outcome1:", outcome)
-                    # print("result1:", result)
-                elif key =='and':
-                    # print("result2bef:", result)
+                elif key == 'and':
                     result = (outcome and result)
-                    # print("outcome2:", outcome)
-                    # print("result2:", result)
                 else:
-                    print("Non-exist key")
+                    print("Non-existing key")
             return result
         else:
             print("It should be list!")
@@ -56,9 +52,9 @@ def solve(rule):
 with open("./folder/"+"test.yml", "r") as testfile:
     try:
         F= yaml.load(testfile, Loader=yaml.SafeLoader)
+        print("F length:", len(F))
         if len(F) > 1:
-            raise ParseError #problem: calls TypeError instead
-        print("F:", len(F))
+            raise RuleLengthError
         print("F[0]:", F[0])
         print("======================")
         print(solve(F[0]))
@@ -66,7 +62,9 @@ with open("./folder/"+"test.yml", "r") as testfile:
         print("I cannot find any rules!")
     except FileNotFoundError:
         print("Please double check the file name!")
-    except ParseError:
+    except RuleLengthError:
         print("Please input one rule at a time!")
+    except ValueError:
+        print("Invalid value!")
     except:
         print("Rule syntax error! Please re-check the .yml file content.")
