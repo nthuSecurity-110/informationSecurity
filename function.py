16f1123b2db49a6807f824b48756a3f4367b93d9
jose <jmyapple9@gmail.com>
2022-07-17 05:37:53 -0400

    rootme function becomes magic function

--- function.py
+++ function.py
@@ -1,6 +1,7 @@
 from inspect import Parameter
 import os
 import getpass
+from matplotlib.pyplot import hist
 from numpy import mat
 from packaging import version
 from subprocess import Popen, PIPE
@@ -10,18 +11,24 @@ import sys
 
 Default = False
 
-def get_output_data(outputLines, Data, block_Out):
+def concatenate_cmd(args):
+    cmd = ""
+    for e in args:
+        cmd += e + " "
+    return cmd
+
+def get_output_data(Data, block_Out):
     for para in block_Out:
         try: # if Data doesn't contain para, we give it as None
             Data[para]
         except KeyError:
             Data[para] = None
         if Data[para] == None:
-            user_input = input("please find value of \"{para}\" in above output, and enter it:".format(para=para))
+            user_input = input("please find value of \"{para}\" in above output, and enter it (true/false):".format(para=para))
             if user_input != '':
-                if user_input == 'True' or user_input == 'true':
+                if user_input.lower()[0] == 't':
                     Data[para] = True
-                elif user_input == 'False' or user_input == 'false':
+                elif user_input.lower()[0] == 'f' :
                     Data[para] = False
                 else:
                     Data[para] = user_input
@@ -35,12 +42,17 @@ def check_output_data(Data, block_Out):
             match = False
     return match
 
-def give_hint(hints):
+def give_hint(hints, args):
     # flush input buffer, in case there are any unexpected user input before that affect input()
     tcflush(sys.stdin, TCIFLUSH)
-    for hint in hints:
-        print(hint)
-        user_input = input('press Enter to continue...\n')
+    # user_input = input('press Enter to continue...\n')
+    cmd = concatenate_cmd(args)
+    if hints!=None:
+        for hint in hints:
+            if ("<cmd>") in hint:
+                hint =  hint.replace("<cmd>", cmd)
+            print(hint)
+            user_input = input('press Enter to continue...\n')
 
 class Function():
     def http_version(func_in, Data, args, block_In, block_Out, block_hint):
@@ -141,7 +153,7 @@ class Function():
         print("class chain is running~~")
         match = check_output_data(Data, block_Out)
         return Data, match
-
+    '''
     def gobuster(func_in, Data, args, block_In, block_Out, block_hint):
         proc = Popen(['gobuster', 'dir', '-u', func_in['IP'], '-w', '/usr/share/wordlists/dirb/common.txt'], stdout=PIPE)
         for stdout_line in iter(proc.stdout.readline, b''):
@@ -155,6 +167,15 @@ class Function():
         match = check_output_data(Data, block_Out)
         # return Data, match
 
+    def netcat(func_in, Data, args, block_In, block_Out, block_hint):
+        cmd = ""
+        for e in args:
+            cmd += e + " "
+        print(f'\nPlease open another terminal and enter `nc {cmd}{func_in["port"]}`.')
+        result = input("After done, press enter to move on next step.\n")
+        match = check_output_data(Data, block_Out)
+        return Data, match
+    '''
     def create_file(func_in, Data, args, block_In, block_Out, block_hint):
         try:
             with open(f'/home/{getpass.getuser()}/Desktop/reverse_shell.php5', 'w') as f:
@@ -173,18 +194,7 @@ class Function():
         match = check_output_data(Data, block_Out)
         return Data, match
 
-    def netcat(func_in, Data, args, block_In, block_Out, block_hint):
-        # os.system(f"nc {args} {func_in['port']}")
-        # proc = Popen(['nc', *args, func_in['port']], stdout=PIPE)
-        # print(f"Start listening to port {func_in['port']}.")
-        cmd = ""
-        for e in args:
-            cmd += e + " "
-        print(f'\nPlease open another terminal and enter `nc {cmd}{func_in["port"]}`.')
-        result = input("After done, press enter to move on next step.\n")
-        match = check_output_data(Data, block_Out)
-        return Data, match
-
+    '''
     def get_root(func_in, Data, args, block_In, block_Out, block_hint):
         print('\nPlease enter `find / -user root -perm -4000 -exec ls -ldb {} \; | grep root` in shell you got.')
         result = input("If you see python has root permission, enter yes, else enter no.\n")
@@ -194,21 +204,25 @@ class Function():
             print("\nSorry we can't help :(\n")
         match = check_output_data(Data, block_Out)
         return Data, match
-
+    '''
     def magic_function(func_in, Data, args, block_In, block_Out, block_hint):
         for i in range(len(args)):
             for input_token in func_in:
                 if ("<" + input_token + ">") in args[i]:
                     args[i] =  args[i].replace("<" + input_token + ">", Data[input_token])
-        print("in magic_function excute:", args)
-        proc = Popen(args, stdout=PIPE)
-        temp = open('temp.txt', 'w')
-        temp.truncate(0)
-        for stdout_line in iter(proc.stdout.readline, b''):
-            print("{}".format(stdout_line.decode('utf-8')).rstrip()) 
-            temp.write("{}\n".format(stdout_line.decode('utf-8')).rstrip())
-        give_hint(block_hint)
-        Data = get_output_data(temp, Data, block_Out)
+        if args!=None and args!=[]:
+            if  args[0]!='NOEXE' :
+                print("in magic_function excute:", concatenate_cmd(args))
+                proc = Popen(args, stdout=PIPE)
+                temp = open('temp.txt', 'w')
+                temp.truncate(0)
+                for stdout_line in iter(proc.stdout.readline, b''):
+                    print("{}".format(stdout_line.decode('utf-8')).rstrip()) 
+                    temp.write("{}\n".format(stdout_line.decode('utf-8')).rstrip())
+            else:
+                args.remove('NOEXE')
+        give_hint(block_hint, args)
+        Data = get_output_data(Data, block_Out)
         match = check_output_data(Data, block_Out)
         return Data, match
 
